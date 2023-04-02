c3b17092c5d734b0cce25022eddc8f56697b4624
gon <gongorgojo@gmail.com>
2023-04-02 20:41:32 -0300

    feat(education): added unit test to education module

--- src/education/education.controller.spec.ts
+++ src/education/education.controller.spec.ts
@@ -1,20 +1,68 @@
+import { createMock } from '@golevelup/nestjs-testing';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { CreateEducationDto } from './dto/createEducation.dto';
+import { EducationOutputDto } from './dto/educationOutput.dto';
+import { UpdateEducationDto } from './dto/updateEducation.dto';
 import { EducationController } from './education.controller';
 import { EducationService } from './education.service';
 
 describe('EducationController', () => {
   let controller: EducationController;
 
+  const fakeDataSrc: Partial<DataSource> = {};
+  const fakeEducationService: Partial<EducationService> = {
+    createEducation: jest
+      .fn()
+      .mockReturnValue(createMock<EducationOutputDto>()),
+    getEducation: jest.fn().mockReturnValue([createMock<EducationOutputDto>()]),
+    updateEducation: jest
+      .fn()
+      .mockReturnValue(createMock<EducationOutputDto>()),
+    deleteEducation: jest.fn().mockReturnValue(''),
+  };
+
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EducationController],
-      providers: [EducationService],
+      providers: [
+        { provide: EducationService, useValue: fakeEducationService },
+        {
+          provide: DataSource,
+          useValue: fakeDataSrc,
+        },
+      ],
     }).compile();
 
     controller = module.get<EducationController>(EducationController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Should create one Education', async () => {
+    const createdEducation = await controller.createEducation(
+      createMock<CreateEducationDto>(),
+    );
+    expect(createdEducation).toBeDefined();
+    expect(fakeEducationService.createEducation).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should get all Education from one User from DB', async () => {
+    const allEducation = await controller.getEducation('1');
+    expect(allEducation).toBeDefined();
+    expect(fakeEducationService.getEducation).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should update one Education from DB', async () => {
+    const updatedEducation = await controller.updateEducation(
+      createMock<UpdateEducationDto>(),
+    );
+    expect(updatedEducation).toBeDefined();
+    expect(fakeEducationService.updateEducation).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should delete one Education from DB', async () => {
+    const deletedEducation = await controller.deleteEducation('1');
+    expect(deletedEducation).toBeDefined();
+    expect(fakeEducationService.deleteEducation).toHaveBeenCalledTimes(1);
   });
 });
--- src/education/education.service.spec.ts
+++ src/education/education.service.spec.ts
@@ -1,18 +1,90 @@
+import { classes } from '@automapper/classes';
+import { AutomapperModule } from '@automapper/nestjs';
+import { createMock } from '@golevelup/nestjs-testing';
 import { Test, TestingModule } from '@nestjs/testing';
+import { EducationMapper } from 'src/mapper/education.mapper';
+import { DataSource } from 'typeorm';
+import { CreateEducationDto } from './dto/createEducation.dto';
+import { UpdateEducationDto } from './dto/updateEducation.dto';
 import { EducationService } from './education.service';
+import { Education } from './model/education.entity';
 
 describe('EducationService', () => {
   let service: EducationService;
 
+  const fakeDataSrc = {
+    manager: {
+      findOne: jest.fn(),
+      delete: jest.fn(),
+      save: jest.fn(),
+      find: jest.fn(),
+      findOneBy: jest.fn(),
+      findBy: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EducationService],
+      imports: [
+        AutomapperModule.forRoot({
+          strategyInitializer: classes(),
+        }),
+      ],
+      providers: [
+        EducationService,
+        EducationMapper,
+        { provide: DataSource, useValue: fakeDataSrc },
+      ],
     }).compile();
 
     service = module.get<EducationService>(EducationService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Should create one Education', async () => {
+    fakeDataSrc.manager.findOneBy.mockReturnValueOnce(createMock<Education>());
+
+    fakeDataSrc.manager.save.mockReturnValueOnce(createMock<Education>());
+
+    const createdEducation = await service.createEducation(
+      createMock<CreateEducationDto>(),
+    );
+
+    expect(createdEducation).toBeDefined();
+    expect(fakeDataSrc.manager.findOneBy).toHaveBeenCalledTimes(1);
+    expect(fakeDataSrc.manager.save).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should get all Education from one User from DB', async () => {
+    fakeDataSrc.manager.find.mockReturnValueOnce([createMock<Education>()]);
+
+    const allEducation = await service.getEducation('1');
+
+    expect(allEducation).toBeDefined();
+    expect(fakeDataSrc.manager.find).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should update one Education from DB', async () => {
+    fakeDataSrc.manager.save.mockReturnValueOnce(createMock<Education>());
+    fakeDataSrc.manager.findBy.mockReturnValueOnce(createMock<Education>());
+
+    const updatedEducation = await service.updateEducation(
+      createMock<UpdateEducationDto>(),
+    );
+
+    expect(updatedEducation).toBeDefined();
+    expect(fakeDataSrc.manager.findBy).toHaveBeenCalledTimes(1);
+    expect(fakeDataSrc.manager.save).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should delete one Education from DB', async () => {
+    fakeDataSrc.manager.findOneBy.mockReturnValueOnce(createMock<Education>());
+    fakeDataSrc.manager.delete.mockReturnValueOnce({});
+
+    const deletedEducation = await service.deleteEducation(1);
+
+    expect(deletedEducation).toBeDefined();
+    expect(fakeDataSrc.manager.delete).toHaveBeenCalledTimes(1);
+    expect(fakeDataSrc.manager.findOneBy).toHaveBeenCalledTimes(1);
   });
 });

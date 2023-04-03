fd77d37c32193ff7cb2e1346aa30beb9fbe67533
gon <gongorgojo@gmail.com>
2023-04-02 21:02:11 -0300

    feat(experience): added unit test for experience controller and service

--- src/experience/experience.controller.spec.ts
+++ src/experience/experience.controller.spec.ts
@@ -1,20 +1,70 @@
+import { createMock } from '@golevelup/nestjs-testing';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { CreateExperienceDto } from './dto/createExperience.dto';
+import { ExperienceOutputDto } from './dto/experienceOutput.dto';
+import { UpdateExperienceDto } from './dto/updateExperience.dto';
 import { ExperienceController } from './experience.controller';
 import { ExperienceService } from './experience.service';
 
 describe('ExperienceController', () => {
   let controller: ExperienceController;
 
+  const fakeDataSrc: Partial<DataSource> = {};
+  const fakeExperienceService: Partial<ExperienceService> = {
+    createExperience: jest
+      .fn()
+      .mockReturnValue(createMock<ExperienceOutputDto>()),
+    getExperience: jest
+      .fn()
+      .mockReturnValue([createMock<ExperienceOutputDto>()]),
+    updateExperience: jest
+      .fn()
+      .mockReturnValue(createMock<ExperienceOutputDto>()),
+    deleteExperience: jest.fn().mockReturnValue(''),
+  };
+
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExperienceController],
-      providers: [ExperienceService],
+      providers: [
+        { provide: ExperienceService, useValue: fakeExperienceService },
+        {
+          provide: DataSource,
+          useValue: fakeDataSrc,
+        },
+      ],
     }).compile();
 
     controller = module.get<ExperienceController>(ExperienceController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Should create one Experience', async () => {
+    const createdExperience = await controller.createExperience(
+      createMock<CreateExperienceDto>(),
+    );
+    expect(createdExperience).toBeDefined();
+    expect(fakeExperienceService.createExperience).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should get all Experience from one User from DB', async () => {
+    const allExperiences = await controller.getExperience('1');
+    expect(allExperiences).toBeDefined();
+    expect(fakeExperienceService.getExperience).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should update one Experience from DB', async () => {
+    const updatedExperience = await controller.updateExperience(
+      createMock<UpdateExperienceDto>(),
+    );
+    expect(updatedExperience).toBeDefined();
+    expect(fakeExperienceService.updateExperience).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should delete one Experience from DB', async () => {
+    const deletedEducation = await controller.deleteExperience('1');
+    expect(deletedEducation).toBeDefined();
+    expect(fakeExperienceService.deleteExperience).toHaveBeenCalledTimes(1);
   });
 });
--- src/experience/experience.service.spec.ts
+++ src/experience/experience.service.spec.ts
@@ -1,18 +1,90 @@
+import { classes } from '@automapper/classes';
+import { AutomapperModule } from '@automapper/nestjs';
+import { createMock } from '@golevelup/nestjs-testing';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ExperienceMapper } from 'src/mapper/experience.mapper';
+import { DataSource } from 'typeorm';
+import { CreateExperienceDto } from './dto/createExperience.dto';
+import { UpdateExperienceDto } from './dto/updateExperience.dto';
 import { ExperienceService } from './experience.service';
+import { Experience } from './model/experience.entity';
+
+const fakeDataSrc = {
+  manager: {
+    findOne: jest.fn(),
+    delete: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+    findBy: jest.fn(),
+  },
+};
 
 describe('ExperienceService', () => {
   let service: ExperienceService;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExperienceService],
+      imports: [
+        AutomapperModule.forRoot({
+          strategyInitializer: classes(),
+        }),
+      ],
+      providers: [
+        ExperienceService,
+        ExperienceMapper,
+        { provide: DataSource, useValue: fakeDataSrc },
+      ],
     }).compile();
 
     service = module.get<ExperienceService>(ExperienceService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Should create one Experience', async () => {
+    fakeDataSrc.manager.findOneBy.mockReturnValueOnce(createMock<Experience>());
+
+    fakeDataSrc.manager.save.mockReturnValueOnce(createMock<Experience>());
+
+    const createdExperience = await service.createExperience(
+      createMock<CreateExperienceDto>(),
+    );
+
+    expect(createdExperience).toBeDefined();
+    expect(fakeDataSrc.manager.findOneBy).toHaveBeenCalledTimes(1);
+    expect(fakeDataSrc.manager.save).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should get all Experience from one User from DB', async () => {
+    fakeDataSrc.manager.find.mockReturnValueOnce([createMock<Experience>()]);
+
+    const allExperiences = await service.getExperience('1');
+
+    expect(allExperiences).toBeDefined();
+    expect(fakeDataSrc.manager.find).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should update one Experience from DB', async () => {
+    fakeDataSrc.manager.save.mockReturnValueOnce(createMock<Experience>());
+    fakeDataSrc.manager.findBy.mockReturnValueOnce(createMock<Experience>());
+
+    const updatedExperience = await service.updateExperience(
+      createMock<UpdateExperienceDto>(),
+    );
+
+    expect(updatedExperience).toBeDefined();
+    expect(fakeDataSrc.manager.findBy).toHaveBeenCalledTimes(1);
+    expect(fakeDataSrc.manager.save).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should delete one Experience from DB', async () => {
+    fakeDataSrc.manager.findOneBy.mockReturnValueOnce(createMock<Experience>());
+    fakeDataSrc.manager.delete.mockReturnValueOnce({});
+
+    const deletedExperience = await service.deleteExperience(1);
+
+    expect(deletedExperience).toBeDefined();
+    expect(fakeDataSrc.manager.delete).toHaveBeenCalledTimes(1);
+    expect(fakeDataSrc.manager.findOneBy).toHaveBeenCalledTimes(1);
   });
 });
